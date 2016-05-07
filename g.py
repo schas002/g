692c88c3d46fea282ab9b02b680db09fd128ae95
kckennylau <kc_kennylau@yahoo.com.hk>
2016-05-07 16:33:05 +0800

    Update g.py
    faster prime detection

--- g.py
+++ g.py
@@ -6,8 +6,11 @@ i = input
 ri = raw_input
 
 def pr(n):
-    for x in range(2, n):
-        if n % x == 0:
+    if n < 2:
+        return False
+    trial = 2
+    while trial * trial <= n:
+        if n % trial == 0:
             return False
     else:
         # loop fell through without finding a factor
