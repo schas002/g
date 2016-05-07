07b4c6fa49868d7b3b2a3e0be7b891496407fcd2
kckennylau <kc_kennylau@yahoo.com.hk>
2016-05-07 18:45:04 +0800

    Update g.py
    Add (prime) factorization and factorial

--- g.py
+++ g.py
@@ -15,3 +15,29 @@ def pr(n):
         trial += 1
     # loop fell through without finding a factor
     return True
+
+#usage:
+#f(48) returns [2,2,2,2,3]
+#f(-5) returns 120
+def f(n):
+    if n<0:
+        n = -n
+        result = 1
+        for i in range(2,n+1):
+            result *= i
+        return result
+    if n<2:
+        return []
+    trial = 2
+    output = []
+    while n > 1:
+        while n % trial == 0:
+            output += [trial]
+            n //= trial
+        trial += 1
+    if n > 1:
+        return output + [n]
+    else:
+        return output
+
+g = math.gamma
